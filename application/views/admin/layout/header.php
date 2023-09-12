c7db5ecc4ac866ffc9baaa8a6b6781591c9d39c3
harkiramadhan <harkiramadhan@gmail.com>
2023-09-12 17:14:14 +0700

    updates

--- application/views/admin/layout/header.php
+++ application/views/admin/layout/header.php
@@ -40,7 +40,7 @@
 			<div class="sidenav-header">
 				<i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
 				<a class="navbar-brand m-0" href="<?= site_url('admin/dashboard') ?>" target="_blank">
-					<img src="../assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
+					<img src="<?= base_url('assets/img/logo-ct-dark.png') ?>" class="navbar-brand-img h-100" alt="main_logo">
 					<span class="ms-1 font-weight-bold">Argon Dashboard 2</span>
 				</a>
 			</div>
